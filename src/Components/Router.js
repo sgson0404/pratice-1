aef2fc4989103bc977a5fa3f67b8f2fcfb5aa1ae
ssgson <gosling.ssg@gmail.com>
2021-03-17 20:02:41 +0900

    router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 import Home from "Routes/Home";
 import TV from "Routes/TV";
